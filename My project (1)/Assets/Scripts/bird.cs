6b2c8f35a5545e3e528c2979df321d11e9febfd1
729889-creator <729889@student.dusd.net>
2025-12-10 13:49:46 -0800

    added animation
    added animations to bird

--- My project (1)/Assets/Scripts/bird.cs
+++ My project (1)/Assets/Scripts/bird.cs
@@ -6,12 +6,13 @@ public class bird : MonoBehaviour
 {
     private bool isDead = false; 
     private Rigidbody2D rb2d;
-
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         // Initialize the rb2d field using GetComponent<Rigidbody2D>()
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,8 +22,18 @@ public class bird : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))  // Add your logic here for when the bird is not dead
             {
-                // Your logic here
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, 200));
+                anim.SetTrigger ("Flap");
             }
         }
+    }
+
+    // Correct Unity collision event method
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;
+        anim.SetTrigger ("Die");
+        GameControl.instance.birdDied();    
     }
 }
